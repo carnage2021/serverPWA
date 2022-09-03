3923b49da116645d0f991beb7c3ea2775dbc0184
carnage2021 <92257777+carnage2021@users.noreply.github.com>
2022-09-03 15:21:35 +0530

    Delete server directory

--- server/controller/testimonialController.js
+++
@@ -1,9 +0,0 @@
-const Testimonial = require('../models/testimonialModel');
-const factory = require('./handlerFactory');
-
-// Controllers
-exports.createTestimonial = factory.createOne(Testimonial);
-exports.getAllTestimonials = factory.getAll(Testimonial);
-exports.getTestimonial = factory.getOne(Testimonial);
-exports.updateTestimonial = factory.updateOne(Testimonial);
-exports.deleteTestimonial = factory.deleteOne(Testimonial);
